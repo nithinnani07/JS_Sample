b93bc991d4914e0c693aefa08ba9fe397280f659
nithinnani07 <34380304+nithinnani07@users.noreply.github.com>
2021-09-30 11:54:06 +0530

    Update StartProcess.js

--- StartProcess.js
+++ StartProcess.js
@@ -1,3 +1,16 @@
 function RPA(){
-	alert('test');
+   	let arguments = {};
+	const robot = UiPathRobot.init();
+	robot.getProcesses()
+		.then(process => {
+  			let calculatorProcess = processes.find(p => p.name.includes('Message'));
+  			let job = new job(calculatorProcess.id, arguments);
+  			robot.startJob(job).then(result => {
+    				console.log(result);
+  			}, err => {
+    				console.log(err);
+  			})
+	}, err => {
+  		console.log(err);
+	});
 }
